b6c9ef5564e81db2ae693c9732b3acaa4208e62d
Prithviraj Khelkar <pkhelkar26@gmail.com>
2023-03-17 12:53:12 -0400

    Updates

---
+++ precache-manifest.71d6bd213f197ea9fb749795918c57af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87cc8ca3ab4c1baea94e",
+    "url": "/static/css/main.2395b7c3.chunk.css"
+  },
+  {
+    "revision": "87cc8ca3ab4c1baea94e",
+    "url": "/static/js/main.87cc8ca3.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "91b1bce54f8bc1a12a66285a242da91c",
+    "url": "/index.html"
+  }
+];
